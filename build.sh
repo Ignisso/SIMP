599671ed28b6915134af48b14bfaaa66cf913f63
mmatusz2 <01168941@pw.edu.pl>
2022-05-22 23:29:44 +0200

    Edit README.md

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 cd "src/main/java"
 echo "Compiling .java files ..."
-for f in $(find . -name '*.java'); do 
+for f in $(find . -name "*.java"); do 
 	echo $f >> sources.list
 done
 javac @sources.list
 rm sources.list
 echo "Packing .class files into .jar..."
-for f in $(find . -name '*.class'); do 
+for f in $(find . -name "*.class"); do 
 	echo $f >> sources.list
 done
 jar cmf MANIFEST.mf ../../../target/Editor.jar @sources.list
